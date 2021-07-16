941bb7409e1a79a405d1ee0a3f660fa0a71e3b14
siddharthadas15 <sd78268@gmail.com>
2021-07-16 22:45:42 +0530

    start script added

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+if (process.env.NODE_ENV !== "production") {
+   require('dotenv').config();
+}
+
 var express=require("express"),
-passport   =require("passport"),
-flash      =require("connect-flash"),
+passport=require("passport"),
+flash=require("connect-flash"),
 methodOverride=require("method-override"),
 LocalStrategy =require("passport-local");
 var app=express();
@@ -9,18 +13,40 @@ app.set("view engine","ejs");
 var bparser=require("body-parser");
 var User =require("./models/user");
 var mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost/yelp_camp");
+var session=require('express-session');
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(dbUrl);
 var Campground=require("./models/campground"),
 Comment=require("./models/comment");
 var seedDB=require("./seed");
 //seedDB();//seed the database
-app.use(bparser.urlencoded({extended:true}));
+const MongoDBStore = require('connect-mongo')(session);
+
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(flash());
-app.use(require("express-session")({
-    secret:"SD15's encode and decode mesaage",
+
+const secret=process.env.SECRET||"SD15's encode and decode mesaage";
+const store = new MongoDBStore({
+   url: dbUrl,
+   secret,
+   touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+   console.log("SESSION STORE ERROR", e)
+});
+app.use(session({
+   store,
+    secret,
     resave:false,
-    saveUninitialized:false
+    saveUninitialized:false,
+    cookie: {
+      httpOnly: true,
+      // secure: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7
+  }
 }));
 app.use(passport.initialize());
 app.use(passport.session());
